85e71afc9084d6577240f6c78b939a80259f0f1c
rei <raihanryuichi@gmail.com>
2024-12-23 05:34:41 +0700

    adjust

--- config.h
+++ config.h
@@ -54,21 +54,26 @@ static char *goto_ws9[] = {"9", NULL};
 /* ========================= KEYBIND ========================= */
 
 static tfwm_keybind_t keybinds[] = {
-    {MOD_KEY | MOD_SHIFT, 0x0071, tfwm_exit, NULL},   /* 0x0071 = q */
-    {MOD_KEY, 0x0071, tfwm_kill, NULL},               /* 0x0071 = q */
-    {MOD_KEY, 0xff0d, tfwm_spawn, cmd_term},          /* 0xff0d = Return */
-    {MOD_KEY, 0x0031, tfwm_goto_workspace, goto_ws1}, /* 0x0031 = 1 */
-    {MOD_KEY, 0x0032, tfwm_goto_workspace, goto_ws2}, /* 0x0032 = 2 */
-    {MOD_KEY, 0x0033, tfwm_goto_workspace, goto_ws3}, /* 0x0033 = 3 */
-    {MOD_KEY, 0x0034, tfwm_goto_workspace, goto_ws4}, /* 0x0034 = 4 */
-    {MOD_KEY, 0x0035, tfwm_goto_workspace, goto_ws5}, /* 0x0035 = 5 */
-    {MOD_KEY, 0x0036, tfwm_goto_workspace, goto_ws6}, /* 0x0036 = 6 */
-    {MOD_KEY, 0x0037, tfwm_goto_workspace, goto_ws7}, /* 0x0037 = 7 */
-    {MOD_KEY, 0x0038, tfwm_goto_workspace, goto_ws8}, /* 0x0038 = 8 */
-    {MOD_KEY, 0x0039, tfwm_goto_workspace, goto_ws9}, /* 0x0039 = 9 */
+    /* Application */
+    {MOD_KEY | MOD_SHIFT, 0x0071, tfwm_exit, NULL}, /* 0x0071 = q */
+    {MOD_KEY, 0x0071, tfwm_kill, NULL},             /* 0x0071 = q */
+    {MOD_KEY, 0xff0d, tfwm_spawn, cmd_term},        /* 0xff0d = Return */
+
+    /* Workspace Navigation */
     {MOD_KEY, 0x005d, tfwm_next_workspace, NULL}, /* 0x005d = bracketright */
     {MOD_KEY, 0x005b, tfwm_prev_workspace, NULL}, /* 0x005b = bracketleft */
-    {MOD_KEY, 0x0060, tfwm_swap_prev_workspace, NULL},    /* 0x0060 = grave */
+    {MOD_KEY, 0x0060, tfwm_swap_prev_workspace, NULL}, /* 0x0060 = grave */
+    {MOD_KEY, 0x0031, tfwm_goto_workspace, goto_ws1},  /* 0x0031 = 1 */
+    {MOD_KEY, 0x0032, tfwm_goto_workspace, goto_ws2},  /* 0x0032 = 2 */
+    {MOD_KEY, 0x0033, tfwm_goto_workspace, goto_ws3},  /* 0x0033 = 3 */
+    {MOD_KEY, 0x0034, tfwm_goto_workspace, goto_ws4},  /* 0x0034 = 4 */
+    {MOD_KEY, 0x0035, tfwm_goto_workspace, goto_ws5},  /* 0x0035 = 5 */
+    {MOD_KEY, 0x0036, tfwm_goto_workspace, goto_ws6},  /* 0x0036 = 6 */
+    {MOD_KEY, 0x0037, tfwm_goto_workspace, goto_ws7},  /* 0x0037 = 7 */
+    {MOD_KEY, 0x0038, tfwm_goto_workspace, goto_ws8},  /* 0x0038 = 8 */
+    {MOD_KEY, 0x0039, tfwm_goto_workspace, goto_ws9},  /* 0x0039 = 9 */
+
+    /* Workspace Layout */
     {MOD_KEY, 0x0074, tfwm_workspace_use_tiling, NULL},   /* 0x0074 = t */
     {MOD_KEY, 0x0066, tfwm_workspace_use_floating, NULL}, /* 0x0066 = f */
     {MOD_KEY, 0x0077, tfwm_workspace_use_window, NULL},   /* 0x0077 = w */
