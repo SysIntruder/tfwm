ca8bcd97bf37a32fe9d6d22b82e0ee8050ae2692
rei <raihanryuichi@gmail.com>
2024-12-23 05:31:50 +0700

    mode default workspace

--- config.h
+++ config.h
@@ -32,9 +32,9 @@
 /* ======================== WORKSPACES ======================= */
 
 static tfwm_workspace_t workspaces[] = {
-    {TILING, "1", 0},
-    {FLOATING, "2", 0},
-    {WINDOW, "3", 0},
+    {TILING, "1", 0}, {WINDOW, "2", 0}, {FLOATING, "3", 0},
+    {TILING, "4", 0}, {TILING, "5", 0}, {TILING, "6", 0},
+    {TILING, "7", 0}, {TILING, "8", 0}, {TILING, "9", 0},
 };
 
 /* ========================= COMMAND ========================= */
@@ -44,6 +44,12 @@ static char *cmd_term[] = {"st", NULL};
 static char *goto_ws1[] = {"1", NULL};
 static char *goto_ws2[] = {"2", NULL};
 static char *goto_ws3[] = {"3", NULL};
+static char *goto_ws4[] = {"4", NULL};
+static char *goto_ws5[] = {"5", NULL};
+static char *goto_ws6[] = {"6", NULL};
+static char *goto_ws7[] = {"7", NULL};
+static char *goto_ws8[] = {"8", NULL};
+static char *goto_ws9[] = {"9", NULL};
 
 /* ========================= KEYBIND ========================= */
 
@@ -54,6 +60,12 @@ static tfwm_keybind_t keybinds[] = {
     {MOD_KEY, 0x0031, tfwm_goto_workspace, goto_ws1}, /* 0x0031 = 1 */
     {MOD_KEY, 0x0032, tfwm_goto_workspace, goto_ws2}, /* 0x0032 = 2 */
     {MOD_KEY, 0x0033, tfwm_goto_workspace, goto_ws3}, /* 0x0033 = 3 */
+    {MOD_KEY, 0x0034, tfwm_goto_workspace, goto_ws4}, /* 0x0034 = 4 */
+    {MOD_KEY, 0x0035, tfwm_goto_workspace, goto_ws5}, /* 0x0035 = 5 */
+    {MOD_KEY, 0x0036, tfwm_goto_workspace, goto_ws6}, /* 0x0036 = 6 */
+    {MOD_KEY, 0x0037, tfwm_goto_workspace, goto_ws7}, /* 0x0037 = 7 */
+    {MOD_KEY, 0x0038, tfwm_goto_workspace, goto_ws8}, /* 0x0038 = 8 */
+    {MOD_KEY, 0x0039, tfwm_goto_workspace, goto_ws9}, /* 0x0039 = 9 */
     {MOD_KEY, 0x005d, tfwm_next_workspace, NULL}, /* 0x005d = bracketright */
     {MOD_KEY, 0x005b, tfwm_prev_workspace, NULL}, /* 0x005b = bracketleft */
     {MOD_KEY, 0x0060, tfwm_swap_prev_workspace, NULL},    /* 0x0060 = grave */
